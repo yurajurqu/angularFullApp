673c3a994802d0b8dc727a38ae34b65ae11875ef
obarboza <obarboza@pe.ibm.com>
2021-04-09 13:46:49 -0500

    environment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "",
+    authDomain: "ng-fitness-tracker-ecede.firebaseapp.com",
+    projectId: "ng-fitness-tracker-ecede",
+    storageBucket: "ng-fitness-tracker-ecede.appspot.com",
+    messagingSenderId: "",
+    appId: ""
+  }
 };
